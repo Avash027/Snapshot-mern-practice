95cf9d2bdbb53cbc450c2088ecf06251ed6013b4
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2021-02-13 22:25:53 +0530

    Project setup

---
+++ server/controllers/posts.js
@@ -0,0 +1,21 @@
+import PostMessage from "../model/postMessages.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ Message: "Error" });
+  }
+};
+
+export async function createPost(req, res) {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.send(409).json({ Message: "Error" });
+  }
+}
